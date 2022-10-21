666c4ce22a64f7c84d1997a80e9964d05c7ccbd2
Soniya Karamchandani <soniyaKaramchandani02@gmail.com>
2022-10-21 19:30:15 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login/login";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signup from "./components/Signup/signup";
-
+import Home from './components/Home/home'
 function App() {
   return (
-    <>
-      <Router>
-        <Switch>
-          
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-
-
-      </Router>
-    </>
+    <Router>
+    
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/signup" element={<Signup/>}/>
+        {/* <Route exact path="/recovery-password" element={<RecoveryPassword/>}/> */}
+        {/* <Route path="*" element={<NotFound/>}/> */}
+      </Routes>
+  
+  </Router>
   );
 }
 
---
+++ src/components/Home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Navbar/navbar'
+// src\components\Home\home.js
+
+function home() {
+    return (
+        <>
+        <Navbar/>
+        <h1>ABOUT SCS</h1>
+        </>
+      );
+}
+
+export default home
