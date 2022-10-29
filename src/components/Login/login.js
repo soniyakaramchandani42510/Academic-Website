18a357e459521cc700917f167fc514caf09ba553
Kanika <kanika.bansal.met20@itbhu.ac.in>
2022-10-29 12:03:26 +0530

    loginpage

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,71 +1,52 @@
 import React from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './login.css';
-// import {useState} from 'react';
-// import axios from "axios";
-import {IconButton} from "@material-ui/core";
-import EmailIcon from '@mui/icons-material/Email';
+import {useState} from 'react';
+import axios from "axios";
+// import {IconButton} from "@material-ui/core";
+// import EmailIcon from '@mui/icons-material/Email';
 
 
 
 function Login() {
-  // const [email,setEmail] = useState('');
-  // const [password,setPassword] = useState(''); 
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState(''); 
 
-  // const handleEmail = (e)=>{
-  //   setEmail(e.target.value)
-  // }
+  const handleEmail = (e)=>{
+    setEmail(e.target.value)
+  }
 
-  // const handlePassword = (e)=>{
-  //   setPassword(e.target.value)
-  // }
-  // const handleApi = ()=>{
-  //   axios.post('',{
-  //     email: email,
-  //     password: password
-  //   })
-  //   .then(result => {
-  //     console.log(result.data)
-  //     alert('success')
-  //   })
-  //   .catch(error => {
-  //     console.log(error)
-  //     alert('service error')
-  //   })
+  const handlePassword = (e)=>{
+    setPassword(e.target.value)
+  }
+  const handleApi = ()=>{
+    axios.post('',{
+      email: email,
+      password: password
+    })
+    .then(result => {
+      console.log(result.data)
+      alert('success')
+    })
+    .catch(error => {
+      console.log(error)
+      alert('service error')
+    })
+  }
 
   return (
-    <div>
-      <div className='row'>
-        <div className='col'>
-          <IconButton>
-            <EmailIcon/>
-          </IconButton>
-        </div>
-        <div className='col' placeholder='Email ID'></div>
-      </div>
-
-    </div>
-//     <form className="forms">
-//       <h2 className='text-center'>Sign In</h2>
-//       {/* <div className='row'>
-//       <a className='text-center col' href="/login">Sign In</a>
-//       </div> */}
-//       <br />
-//   <div class="mb-3">
-//     <label for="exampleInputEmail1" class="form-label">Email address</label>
-//     <input type="email" value={email} onChange={handleEmail} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-//   </div>
-//   <div class="mb-3">
-//     <label for="exampleInputPassword1" class="form-label">Password</label>
-//     <input type="password" value={password} onChange={handlePassword} class="form-control" id="exampleInputPassword1" placeholder="Enter password" />
-//   </div>
-//   <div class="mb-3 form-check">
-//     <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-//     <label class="form-check-label" for="exampleCheck1">Remember me</label>
-//   </div>
-//   <button type="submit" onClick={handleApi} class="btn btn-primary">Submit</button>
-// </form>
+    <Form className="forms">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" value={email} onChange={handleEmail} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" value={password} onChange={handlePassword} class="form-control" id="exampleInputPassword1" placeholder="Enter password" />
+  </div>
+  <Button type="submit" onClick={handleApi} class="btn btn-primary">LOGIN</Button>
+</Form>
   )
 }
 
